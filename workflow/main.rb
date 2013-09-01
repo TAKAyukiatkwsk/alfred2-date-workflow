594cf2bda2931a0ad9c50129e9ad3305492cdef1
TAKAyuki_atkwsk <takagi.takayuki.yuuki@gmail.com>
2013-09-02 00:38:47 +0900

    Initial commit

---
+++ workflow/main.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
+require "bundle/bundler/setup"
+require "alfred"
+
+
+
+
+Alfred.with_friendly_error do |alfred|
+  fb = alfred.feedback
+
+  puts fb.to_xml(ARGV)
+end
+
+
+
